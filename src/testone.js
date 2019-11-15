dc09736b929bc1bbd55518a0f615d530c9ce5b77
lvzhifu <lvzhifu@beyondsoft.com>
2019-11-15 13:58:21 +0800

    * bug

--- src/testone.js
+++ src/testone.js
@@ -21,9 +21,9 @@ httpd.createServer(async (req,res)=>{
         const page = await browser.newPage();
         page.waitForNavigation({timeout: 48000000})
         // await page.goto('http://localhost/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
-        // await page.goto('http://sg-preview.linkerdeals.com/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
+        await page.goto('http://sg-preview.linkerdeals.com/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
         //  await page.goto('https://www.baidu.com/', {waitUntil: 'networkidle0'});
-        await page.goto('http://sg-qa.bysrd.cn/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
+        // await page.goto('http://sg-qa.bysrd.cn/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
         const html = await page.content(); // serialized HTML of page DOM.
         await browser.close();
         res.write(html);
