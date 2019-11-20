07568c40ecc74c11b3431ebc8ce9d0e3480a50b6
lvzhifu <lvzhifu@beyondsoft.com>
2019-11-20 22:25:43 +0800

    * bug

--- src/testone.js
+++ src/testone.js
@@ -5,23 +5,19 @@ const puppeteer = require('puppeteer');
 
 //创建服务
 httpd.createServer(async (req,res)=>{
-    console.log(req.url)
-    console.log(req.url.indexOf('.'))
     if(req.url.indexOf('.') > -1 && req.url.indexOf('.html') < 0) {
-        console.log('进入判断')
         res.end()    
         return
     }
     const userAgent = req.headers["user-agent"]
-    console.log(userAgent)
     let myUrl = url.parse(req.url, true); 
-    console.log(myUrl)
     try {
         const browser = await puppeteer.launch({timeout: 50000000, args: ['--no-sandbox']});
         const page = await browser.newPage();
         page.waitForNavigation({timeout: 48000000})
         // await page.goto('http://localhost/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
-        await page.goto('http://sg-preview.linkerdeals.com/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
+        await page.goto('https://www.gaietysg.com/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
+        // await page.goto('http://sg-preview.linkerdeals.com/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
         //  await page.goto('https://www.baidu.com/', {waitUntil: 'networkidle0'});
         // await page.goto('http://sg-qa.bysrd.cn/merchant'+ myUrl.search, {waitUntil: 'networkidle0'});
         const html = await page.content(); // serialized HTML of page DOM.
